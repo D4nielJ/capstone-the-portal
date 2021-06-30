9ccf82924c03ce783f8c58bfbfd2f0b6d3191a3f
D4nielJ <d4niel.djm@gmail.com>
2021-06-30 17:12:26 -0500

    Add dynamic section for guests

--- js/main.js
+++ js/main.js
@@ -47,3 +47,59 @@ setInterval(() => {
 }, 300);
 
 // DYNAMIC SECTION FOR GUESTS
+// Guests Object
+
+const guestsArr = [
+  {
+    name: 'Yochai Benkler',
+    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, animi.',
+    imgSrc: './assets/img/guests/guest-1.webp',
+  }, {
+    name: 'Jhon Doe',
+    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, animi.',
+    imgSrc: './assets/img/guests/guest-1.webp',
+  }, {
+    name: 'Annya Taylor Chester',
+    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, animi.',
+    imgSrc: './assets/img/guests/guest-1.webp',
+  }, {
+    name: 'Yin Lao',
+    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, animi.',
+    imgSrc: './assets/img/guests/guest-1.webp',
+  }
+]
+
+// Selectors
+
+const guestsList = document.querySelector('.guests__list');
+
+// Function
+
+const appendGuests = () => {
+  for (let i = 0; i < guestsArr.length; i += 1) {
+    const li = document.createElement('li');
+    li.innerHTML = 
+      `<div class="guests__img-container">
+        <span class="guests__decor-img"></span>
+        <img src="${guestsArr[i].imgSrc}" alt="Photo of the first guest" class="guests__img">
+      </div>
+      <div class="guests__info">
+        <hgroup>
+          <h3 class="guests__name">${guestsArr[i].name}</h3>
+          <h4 class="guests__role">${guestsArr[i].role}</h4>
+        </hgroup>
+        <span class="title-decor title-decor--small"></span>
+        <p class="guests__desc">${guestsArr[i].desc}</p>
+      </div>`;
+    li.classList.add('guests__card')
+    guestsList.appendChild(li);
+  }  
+}
+
+// Call
+
+window.onload = appendGuests;
